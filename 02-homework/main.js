907f036648fe0431a8a29dd25e6ae281862a9bba
Vyacheslav <dyaditsyn@gmail.com>
2020-07-22 01:58:55 +0300

    fixed zero issue

--- 02-homework/main.js
+++ 02-homework/main.js
@@ -3,18 +3,24 @@
 let N, M, choice, sum = 0;
 
 do{
-    N = +prompt("Please enter the lower limit of numbers to sum - must be non-zero integer");
-} while ( (isNaN( N ) ) || (N % 1) )
+    N = +prompt("Please enter the lower limit of numbers to sum - must be integer");
+} while (  !(Number.isInteger(N)) )
+
+console.log("The first number is: " + N)
 
 do{
-    M = +prompt("Please enter the upper limit of numbers to sum - must be non-zero integer bigger than lower");
-} while ( (isNaN( N ) ) || (N % 1) || (M < N) )
+    M = +prompt("Please enter the upper limit of numbers to sum - must be integer bigger than lower");
+} while ( !(Number.isInteger(M)) || (M < N) )
+
+console.log("The second number is: " + M)
 
 choice = confirm("Wish you skip even numbers? OK - YES, Cancel - NO");
 
+console.log("Skip even numbers: " + choice)
+
 for (let i = N; i <= M; i++){
 
-    if (choice){
+    if (choice){ 
 
         if (i % 2){
             sum += i;
@@ -25,6 +31,7 @@ for (let i = N; i <= M; i++){
     }
 } 
 
-console.log(sum)
+console.log("The sum is: " + sum)
 
 alert("Your sum is: " + sum);
+
