fe13c96dad7fc99d270934e28a64eefb0f588928
Vyacheslav <dyaditsyn@gmail.com>
2020-07-22 01:07:23 +0300

    hw2 v1.0

--- 02-homework/main.js
+++ 02-homework/main.js
@@ -1,2 +1,30 @@
 "use strict";
 
+let N, M, choice, sum = 0;
+
+do{
+    N = +prompt("Please enter the lower limit of numbers to sum - must be non-zero integer");
+} while ( (isNaN( N ) ) || (N % 1) )
+
+do{
+    M = +prompt("Please enter the upper limit of numbers to sum - must be non-zero integer bigger than lower");
+} while ( (isNaN( N ) ) || (N % 1) || (M < N) )
+
+choice = confirm("Wish you skip even numbers? OK - YES, Cancel - NO");
+
+for (let i = N; i <= M; i++){
+
+    if (choice){
+
+        if (i % 2){
+            sum += i;
+        };
+        
+    } else{
+        sum += i;
+    }
+} 
+
+console.log(sum)
+
+alert("Your sum is: " + sum);
