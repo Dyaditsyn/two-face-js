b9ea72c95b54c88e45134e6fda7bdb1b4d521221
Vyacheslav <dyaditsyn@gmail.com>
2020-07-22 15:14:08 +0300

    fixed space/enter issues

--- 02-homework/main.js
+++ 02-homework/main.js
@@ -1,16 +1,27 @@
 "use strict";
 
-let N, M, choice, sum = 0;
+let N = 0,
+    M = 0,  
+    sum = 0,
+    choice;
+
+function MyInteger(param){
+    if ( param === "" || param === " " || param === null){
+        return false;
+    } else {
+        return Number.isInteger(+param)
+    }
+}
 
 do{
     N = prompt("Please enter the lower limit of numbers to sum - must be integer");
-} while (  !(Number.isInteger(+N)) || (N === null) )
+} while (  !(MyInteger(N)) )
 
 console.log("The first number is: " + N)
 
 do{
-    M = +prompt("Please enter the upper limit of numbers to sum - must be integer bigger than lower");
-} while ( !(Number.isInteger(+M)) || (M < N) || (N === null))
+    M = prompt("Please enter the upper limit of numbers to sum - must be integer bigger than lower");
+} while ( !(MyInteger(M)) || (+M < +N) )
 
 console.log("The second number is: " + M)
 
